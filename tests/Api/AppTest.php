cdb2ebf2c01a9e1b5d6a7d647aad6a2206c0c617
Marius Schütte <marius.schuette@gmail.com>
2020-03-11 13:36:26 +0100

    Refactored routing, WIP

--- tests/Api/AppTest.php
+++ tests/Api/AppTest.php
@@ -177,7 +177,7 @@ class AppTest extends TestCase
         $this->assertEquals(1, $stack->contains(ErrorMiddleware::class));
     }
 
-    public function testFastRouteMiddleware()
+    public function testRunFastRouteMiddlewareTwice()
     {
         $this->container->addDefinitions(
             [
@@ -266,6 +266,40 @@ class AppTest extends TestCase
         });
         $request = $kernel->get(ServerRequest::class);
         $request = $request->withUri($request->getUri()->withPath('/foo/bar'));
+
+        $response = $app->handle($request);
+        $this->assertEquals(301, $response->getStatusCode());
+        $this->assertEquals('Moved Permanently', $response->getReasonPhrase());
+    }
+
+    public function testNestedRouteGroups()
+    {
+        $this->container->addDefinitions(
+            [
+                'set400' => $this->mockMiddleware(
+                    function (ServerRequest $request, HandlesServerRequests $next) {
+                        return $next->handle($request)->withStatus(StatusCode::STATUS_BAD_REQUEST);
+                    }
+                ),
+                'set301' => $this->mockMiddleware(
+                    function (ServerRequest $request, HandlesServerRequests $next) {
+                        return $next->handle($request)->withStatus(StatusCode::STATUS_MOVED_PERMANENTLY);
+                    }
+                )
+            ]
+        );
+        $kernel = $this->container->build();
+        $app = $kernel->get(App::class);
+        $app->add(FastRouteMiddleware::class);
+        $app->group('/foo', function (Routing\Api $group) {
+            $group->group('/bar', function (Routing\Api $group) {
+                $group->get('/baz', function ($request, $response, $args) {
+                    return $response;
+                });
+            })->add('set400');
+        })->add('set301');
+        $request = $kernel->get(ServerRequest::class);
+        $request = $request->withUri($request->getUri()->withPath('/foo/bar/baz'));
 
         $response = $app->handle($request);
         $this->assertEquals(301, $response->getStatusCode());
